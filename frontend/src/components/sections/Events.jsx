b79bedd4c5355c1a4e038582204c4767cf9d0f73
Noahhuang78 <huangziyi897@gmail.com>
2025-11-14 13:18:37 +0800

    apply by date

--- frontend/src/components/sections/Events.jsx
+++ frontend/src/components/sections/Events.jsx
@@ -4,7 +4,8 @@ export default function Events({ data }) {
   return (
     
     <div className="events-container">
-      <h1>Upcoming Events</h1>
+      <br></br>
+      <h1>Browse Events</h1><br></br>
       <div className="events-wrapper">
         {data.map((event)=> <EventCard key={event.id} category={event.category} organisation={event.organisation} description={event.description} title={event.title} date={event.date} time={event.time} location={event.location} spots={event.spots} imgSrc={`${event.imgSrc}?w=400&h=300&fit=crop&auto=format`}/>)}
 
